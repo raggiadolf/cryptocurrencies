7a5cff800c3b37bb6d33a94365d02d330cdef8ac
annavigdis <annaviru@gmail.com>
2016-12-01 09:09:04 +0000

    Added a zero mask string to validate if string ends with zeros

--- hashparty.py
+++ hashparty.py
@@ -2,46 +2,45 @@ from Crypto.Hash import SHA256
 import time
 from bitarray import bitarray
 
-magic_len = 4
+magic_len = 20
 
-mask = '11111111'
-mask_string = bitarray('0'*248 + mask)
-#print mask_string
+mask = '1'*magic_len
+mask_string = bitarray('0'*(256-magic_len) + mask)
+zero_mask_string = bitarray('0'*256)
+
+print('mask', mask)
 
 k = bitarray(128)
 x = bitarray(128)
 
-e = bitarray(256)
-
 def hashtobits(hash):
-	#return bitarray(bin(int(hash, 16))[2:])
-	i = int(hash, 16)
-	print format(i, '256b')
-	bits = bitarray(format(int(hash, 16), '0256b'))
-	print bits.length()
-	return bits
+  #return bitarray(bin(int(hash, 16))[2:])
+  i = int(hash, 16)
+  # print format(i, '256b')
+  bits = bitarray(format(int(hash, 16), '0256b'))
+  return bits
 
 def testbits(bits):
-	print 'mask_string len: ' + str(len(mask_string))
-	print 'bit len: ' + str(len(bits))
-	result = (mask_string & bits)
-	return result == '00000000'
-
-def test(hashedvalue):
-	return (int(hashedvalue, 16) % (2 ** magic_len)) == 0
+  # print 'mask_string len: ' + str(len(mask_string))
+  # print 'bit len: ' + str(len(bits))
+  result = (mask_string & bits)
+  return result == zero_mask_string
 
 start = time.time()
-for i in range(100000):
-	print 'x length: ' + str(x.length())
-	t = SHA256.new(k + x).hexdigest()
-	#print(hashtobits(t))
-	#if testbits(hashtobits(t)) is True:
-	if testbits(hashtobits(t)) is True:
-		print "Found x: " + str(x)
-		break
-	#x = x + bitarray('1')
-	newx = bin(int(x.to01(), 2) + 1)[2:]
-	print newx
-	x = bitarray(newx)
+for i in range(10000):
+  print 'x length: ', len(x)
+  t = SHA256.new(k + x).hexdigest()
+  hashtobitsstring = hashtobits(t)
+  if testbits(hashtobitsstring):
+    print "Found x: ", x, len(x)
+    print "Number of iterations: ", i
+    print "Hash to bits string: ", hashtobitsstring
+    break
+  x = x + bitarray('1')
+  newx = bin(int(x.to01(), 2) + 1)[2:]
+  # x = x.to01()
+  # newx = '{:0{}b}'.format(long(x, 2) + 1, len(x))
+  x = bitarray(newx)
+
 end = time.time()
 print(end - start)
