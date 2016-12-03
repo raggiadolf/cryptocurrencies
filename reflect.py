516d0de95ec60edb1636afe73ebb14c563f7c427
raggiadolf <ragnar.adolf@gmail.com>
2016-12-03 16:01:10 +0000

    Added RSA encryption to reflection chat. Clients start out exchanging public keys, then they use each others public keys to encrypt the rest of their communication. The reflection server still has hold of the client's public keys so he could change the messages that get sent back and forth. Still need a more robust key exchange protocol.

--- reflect.py
+++ reflect.py
@@ -7,22 +7,37 @@ from Crypto.PublicKey import RSA
 host = ''
 clients = []
 
+def send_client_info(recipient, client):
+	conn.sendto(cPickle.dumps(client), (recipient['host'], recipient['port']))
+
 def reflect(conn, clients):
 	msg = conn.recvfrom(2048)
 	data = msg[0]
 	addr = msg[1]
-	print "Received: (" + msg[0] + ") from: [" + addr[0] + ":" + str(addr[1]) + "]"
+	print "Received: (" + msg[0] + ") from: [" + addr[0] + ":" + str(addr[1]) + "]" # This should be encrypted you pigs!
 	recipient = filter(lambda client: client['host'] != addr[0] or client['port'] != addr[1], clients)
 	sender = filter(lambda client: client['host'] == addr[0] and client['port'] == addr[1], clients)
 	if not recipient:
 		print "Error, can't find recipient?"
 		return
-	msg_to_send = {
-		'msg': msg[0],
-		'client': sender[0]
-	}
-	print "Sending: ", msg_to_send
-	conn.sendto(cPickle.dumps(msg_to_send), (recipient[0]['host'], recipient[0]['port']))
+	#msg_to_send = {
+	#	'msg': msg[0],
+	#	'client': sender[0]
+	#}
+	conn.sendto(data, (recipient[0]['host'], recipient[0]['port']))
+
+def initiate(conn, clients):
+	print "Waiting for first client..."
+	get_init_msg(conn, clients)
+
+	print "Waiting for second client..."
+	get_init_msg(conn, clients)
+
+	print "Both clients connected, distributing client info..."
+	send_client_info(clients[0], clients[1])
+	send_client_info(clients[1], clients[0])
+
+	print "Client info distributed..."
 
 def get_init_msg(conn, clients):
 	msg = conn.recvfrom(2048)
@@ -48,8 +63,10 @@ def get_init_msg(conn, clients):
 port = int(sys.argv[1])
 conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 conn.bind(('', port))
-while len(clients) < 2:
-	get_init_msg(conn, clients)
+
+initiate(conn, clients)
+
+print "Connections initated, starting reflection..."
 
 while True:
 	reflect(conn, clients)
--- reflsend.py
+++ reflsend.py
@@ -10,26 +10,43 @@ rng = Random.new().read
 
 cert_text = "This is a text to sign and verify"
 
-key = RSA.generate(2048, rng)
-public_key = key.publickey()
-signature = key.sign(SHA256.new(cert_text).digest(), rng)
-
 host = '127.0.0.1'
 port = 59191
 
-def decrypt(data):
-	msg_obj = cPickle.loads(data)
-	
+recipient = {}
 
+def gen_key(rng):
+	key = RSA.generate(2048, rng)
+	public_key = key.publickey()
+	signature = key.sign(SHA256.new(cert_text).digest(), rng)
+	return key, public_key, signature
+
+def verify(key, signature):
+	return key.verify(SHA256.new(cert_text).digest(), signature)
+
+def decrypt(msg):
+	return key.decrypt(msg)
+	
 def recv(s):
 	while True:
 		msg = s.recvfrom(2048)
 		data = msg[0]
 		addr = msg[1]
 		if not data: sys.exit(0)
-		print decrypt(data)
+		print "\n" + decrypt(data)
+		sys.stdout.write('>> ')
+		sys.stdout.flush()
+
+def init(s):
+	recipient_obj = cPickle.loads(s.recvfrom(2048)[0])
+
+	print "Received recipient info from server"
+
+	return recipient_obj
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+key, public_key, signature = gen_key(rng)
 
 keysig_object = {
 	'key': public_key.exportKey(),
@@ -38,8 +55,20 @@ keysig_object = {
 
 s.sendto(cPickle.dumps(keysig_object), (host, port))
 
-Thread(target=recv, args=(s,)).start()
+print "Opening secure channel, waiting for recipient to join..."
+recipient = init(s)
+
+print "Verifying..."
+if not verify(recipient['key'], recipient['signature']):
+		print "Key signature not verified, exiting..."
+		sys.exit()
+
+print "Recipient info received and verified. Start chatting away."
+
+t = Thread(target=recv, args=(s,))
+t.daemon = True
+t.start()
 
 while True:
 	msg = raw_input('>> ')
-	s.sendto(msg, (host, port))
+	s.sendto(recipient['key'].encrypt(msg, 32)[0], (host, port))
