7ea668c990fcf6a532a1493b98749c47cc8a43aa
annavigdis <annaviru@gmail.com>
2016-12-05 14:52:07 +0000

    Added a authorization of the transaction and an incomplete verification of the receiver and sender

--- bigbrotherbank.py
+++ bigbrotherbank.py
@@ -1,56 +1,113 @@
 import socket
 import sys
 import json
+import uuid
 
-def verify():
+def verify(verify_obj):
   return {
-    'success': True
+    'success': success
   }
 
-def authorize():
-  # check if payer has enough money
+def authorize(auth_obj):
+  with open('config.json') as data_file:
+    config = json.load(data_file)
+  clients = config['clients']
+  payer_id = auth_obj['payer_id']
+  payer_balance = clients[payer_id]['amount']
+  print 'payer balance', payer_balance, 'amount to charge', auth_obj['amount']
+
+  success = False
+  if payer_balance >= auth_obj['amount']:
+    success = True
+    # TODO: check if this transaction number is already in the "table"
+    transaction_id = str(uuid.uuid4())
+    transferFunds(auth_obj, transaction_id)
+    return {
+      'success': True,
+      'transaction_id': transaction_id
+    }
+  else:
+    return {
+      'success': False
+    }
+
+def verifyClients(data):
+  with open('config.json') as data_file:
+    config = json.load(data_file)
+  clients = config['clients']
+  payer_id = data['payer_id']
+  receiver_id = data['receiver_id']
+  # if config[receiver_id] or config[payer_id] and receiver_id != payer_id:
+  # for client in cl:
+     # print r.get('key_name')
+  # isVerified = False
+  # for r in results:
+  #   if 'receiver_id' in r or 'payer_id' in r:
+  # if clients[receiver_id]:
+  #   print('receiver_id config', config['receiver_id'])
+  #   return True
+  # else:
+  #   print 'no match'
+  #   return False
+
+def transferFunds(auth_obj, transaction_id):
+  with open('config.json', 'r') as f:
+    config = json.load(f)
+    # exchange funds
+    config['clients'][auth_obj['payer_id']]['amount'] = config['clients'][auth_obj['payer_id']]['amount'] - auth_obj['amount']
+    config['clients'][auth_obj['receiver_id']]['amount'] = config['clients'][auth_obj['payer_id']]['amount'] + auth_obj['amount']
+    # store transaction
+    config['transactions'][transaction_id] = createTransactionObject(auth_obj)
+
+  with open('config.json', 'w') as f:
+    f.write(json.dumps(config))
+
+def createTransactionObject(auth_obj):
   return {
-    'success': True,
-    'transaction_id': 1
+    "amount": auth_obj['amount'],
+    "payer_id": auth_obj['payer_id'],
+    "receiver_id": auth_obj['receiver_id']
   }
 
 def main():
   host = ''
   port = 59191
 
-  with open('config.json') as data_file:
-      config = json.load(data_file)
-
   try:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    print('socket created')
+    print 'socket created'
   except socket.error as msg:
-    print('Failed to create socket. Error code: ', str(msg[0]), ' Message: ', msg[1])
+    print 'Failed to create socket. Error code: ', str(msg[0]), ' Message: ', msg[1]
     sys.exit()
 
   try:
     s.bind((host, port))
   except socket.error as msg:
-    print('Failed to create socket. Error code: ', str(msg[0]), ' Message: ', msg[1])
+    print 'Failed to create socket. Error code: ', str(msg[0]), ' Message: ', msg[1]
 
-  print('Socket bind complete')
+  print 'Socket bind complete'
 
   while(True):
     d = s.recvfrom(4096)
-    print('d', d)
     data = json.loads(d[0].decode('utf-8'))
     addr = d[1]
 
-
-    response = ''
-    if not data.get('transaction_id'):
-      response = authorize()
-    else:
-      response = verify()
-
     if not data:
       break
-    print('response', response)
+
+    print('data..', data)
+
+    response = ''
+    # if verifyClients(data):
+    if not data.get('transaction_id'):
+      response = authorize(data)
+    else:
+      response = verify(data)
+    # else:
+    #   response = {
+    #     'success': False
+    #   }
+
     s.sendto(json.dumps(response), addr)
 
 main()
